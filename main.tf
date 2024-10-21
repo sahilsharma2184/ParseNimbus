b0eeccc913d85006cff851bb315a9f364dfcef5b
sahilsharma2184 <sahilsharma2184@gmail.com>
2024-10-21 17:41:39 +0530

    Terraform Script

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 # Creation of ec2 instance
-resource "aws_instance" "myways_instance" { 
+resource "aws_instance" "parsenimbus_instance" { 
     ami ="ami-0866a3c8686eaeeba" # Amazon Machine Image of Ubuntu to launch ec2 instance.
     instance_type = "t2.micro"  # Selecting t2.micro as the instance type, it is free-tier eligible.
-    key_name = "ec2_myways_KP" # The key pair name used to securely ssh into the ec2 instance.
+    key_name = "ec2_parsenimbus_KP" # The key pair name used to securely ssh into the ec2 instance.
     vpc_security_group_ids = ["sg-058d2104aa47f4ad3"] # The security group to control traffic for the ec2 instance
     tags = {
       Name = "ParseNimbus" # Tags the EC2 instance with a name for easier identification, here 'ParseNimbus'
